41c3d82e286a0f8cb549e841fe0fbfc0ad460c0b
Samarth Kamble <kamblesamarth04@gmail.com>
2024-09-15 19:26:03 +0530

    Initilize app

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,10 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const generalSans = localFont({
+  src: "./fonts/GeneralSans-Variable.ttf",
+  variable: "--font-general-sans",
+  weight: "100 200 3000 400 500 600 700 800 900",
 });
 
 export const metadata = {
@@ -20,11 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${generalSans.variable} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
+        <div className='max-w-7xl mx-auto'>
+            <div className='flex justify-between items-center py-5 mx-auto c-space'>
+
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
