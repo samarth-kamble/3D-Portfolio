3d81faef71c625b0000e24e3f69c2d737cfe50ea
Samarth Kamble <kamblesamarth04@gmail.com>
2024-09-16 14:07:37 +0530

    Implement the about section

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 import Hero from "@/components/section/Hero";
 import Navbar from "@/components/section/Navbar";
 import React from "react";
+import About from "@/components/section/About";
 
 const HomePage = () => {
   return (
     <section className="max-w-7xl mx-auto">
       <Navbar />
       <Hero />
+        <About  />
+
     </section>
   );
 };
