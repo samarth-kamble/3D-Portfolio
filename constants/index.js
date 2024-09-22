95bb5640e3512ee93ebe524e8180c1c71f2d6e06
Samarth Kamble <kamblesamarth04@gmail.com>
2024-09-22 22:23:27 +0530

    Project Added

--- constants/index.js
+++ constants/index.js
@@ -23,81 +23,122 @@ export const navLinks = [
 
 export const myProjects = [
   {
-    title: 'Podcastr - AI Podcast Platform',
-    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+    title: "Podcastr - AI Podcast Platform",
+    desc: "Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.",
     subdesc:
-        'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
-    texture: '/textures/project/project1.mp4',
-    logo: '/assets/project-logo1.png',
+      "Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.",
+    href: "https://ai-podcastr-app.vercel.app/",
+    texture: "/textures/project/project1.mp4",
+    logo: "/assets/project-logo1.png",
     logoStyle: {
-      backgroundColor: '#2A1816',
-      border: '0.2px solid #36201D',
-      boxShadow: '0px 0px 60px 0px #AA3C304D',
+      backgroundColor: "#2A1816",
+      border: "0.2px solid #36201D",
+      boxShadow: "0px 0px 60px 0px #AA3C304D",
     },
-    spotlight: '/assets/spotlight1.png',
+    spotlight: "/assets/spotlight1.png",
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
+        name: "React.js",
+        path: "/assets/react.svg",
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
       },
       {
         id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
+        name: "TypeScript",
+        path: "/assets/typescript.png",
       },
       {
         id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        name: "Open AI",
+        path: "/assets/openai.png",
       },
     ],
   },
   {
-    title: 'Imaginify - AI Photo Manipulation App',
-    desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+    title: "Imaginify - AI Photo Manipulation App",
+    desc: "Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.",
     subdesc:
-        'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
-    href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
-    texture: '/textures/project/project5.mp4',
-    logo: '/assets/project-logo5.png',
+      "Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.",
+    href: "https://imaginify-saas-platform.vercel.app/",
+    texture: "/textures/project/project5.mp4",
+    logo: "/assets/project-logo5.png",
     logoStyle: {
-      backgroundColor: '#1C1A43',
-      border: '0.2px solid #252262',
-      boxShadow: '0px 0px 60px 0px #635BFF4D',
+      backgroundColor: "#1C1A43",
+      border: "0.2px solid #252262",
+      boxShadow: "0px 0px 60px 0px #635BFF4D",
     },
-    spotlight: '/assets/spotlight5.png',
+    spotlight: "/assets/spotlight5.png",
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
+        name: "React.js",
+        path: "/assets/react.svg",
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
       },
       {
         id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
+        name: "TypeScript",
+        path: "/assets/typescript.png",
       },
       {
         id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        name: "Framer Motion",
+        path: "/assets/framer.png",
+      },
+      {
+        id: 5,
+        name: "Clerk Auth",
+        path: "/assets/clerk .png",
+      },
+    ],
+  },
+  {
+    title: "CaseCobra - Custom Case App",
+    desc: "Capture your favorite memories with your own, one-of-one phone case. CaseCobra allows you to protect your memories, not just your phone case.",
+    subdesc:
+      "Built with Next.js 14, Uploadthing , Kinde, and Stripe, CaseCobra combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.",
+    href: "https://casecobra-ecommerce-eight.vercel.app/",
+    texture: "/textures/project/casecobra.mp4",
+    logo: "/assets/snake-1.png",
+    logoStyle: {
+      backgroundColor: "#1C1A43",
+      border: "0.2px solid #252262",
+      boxShadow: "0px 0px 60px 0px #635BFF4D",
+    },
+    spotlight: "/assets/spotlight3.png",
+    tags: [
+      {
+        id: 1,
+        name: "React.js",
+        path: "/assets/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "assets/tailwindcss.png",
+      },
+      {
+        id: 3,
+        name: "TypeScript",
+        path: "/assets/typescript.png",
+      },
+      {
+        id: 4,
+        name: "Framer Motion",
+        path: "/assets/framer.png",
       },
     ],
   },
 ];
-
 
 export const calculateSizes = (isSmall, isMobile, isTablet) => {
   return {
